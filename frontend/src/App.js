9054e92330f79c1b7659e45c5fed06ad22ea5176
mariosknl <mariosknl@outlook.com>
2020-12-01 12:09:07 +0200

    react-router-dom react-router-bootstrap implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 

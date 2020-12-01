85ad3a0067c085536de7c15318e50a9942b600dd
mariosknl <mariosknl@outlook.com>
2020-12-01 13:05:08 +0200

    creating environmental variable and using it with dotenv package

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
+
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +19,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);

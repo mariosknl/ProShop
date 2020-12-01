99dd838ce97fa641ff6eb006f417fbc6ff4d2287
mariosknl <mariosknl@outlook.com>
2020-12-01 13:10:16 +0200

    change to import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+
+import products from "./data/products.js";
 
 dotenv.config();
 

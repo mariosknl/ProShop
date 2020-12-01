e916a910313bde37a37e82496817acfd33f338aa
mariosknl <mariosknl@outlook.com>
2020-12-01 15:56:59 +0200

    fetching data from database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,37 @@
+import express from "express";
+import AsyncHandler from "express-async-handler";
+
+import Product from "../models/productModel.js";
+
+const router = express.Router();
+
+// @desc    Fetch all Products
+// @route   Get /api/products
+// @access  Public
+router.get(
+  "/",
+  AsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json(products);
+  })
+);
+
+// @desc    Fetch Single Product
+// @route   Get /api/product/:id
+// @access  Public
+
+router.get(
+  "/:id",
+  AsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  })
+);
+
+export default router;

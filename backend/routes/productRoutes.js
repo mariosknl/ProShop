146f9230a65dc697ac63afc16ae174af42d70eb9
mariosknl <mariosknl@outlook.com>
2020-12-01 19:23:41 +0200

    error handling middleware added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -29,7 +29,8 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: "Product not found" });
+      res.status(404);
+      throw new Error("Product not Found!");
     }
   })
 );

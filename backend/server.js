c896a5ff3cebb01f15a4a91fc26545633256fcd3
mariosknl <mariosknl@outlook.com>
2020-12-01 13:47:42 +0200

    installed colors package for showing messages in the console

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
 
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
@@ -27,5 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );

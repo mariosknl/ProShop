08cec3e5534e3b01f630e9ca6a3476bbc2f19311
mariosknl <mariosknl@outlook.com>
2020-12-04 10:10:44 +0200

    typing error fixed

--- backend/controllers/productContoller.js
+++ backend/controllers/productContoller.js
@@ -1,11 +1,11 @@
-import AsyncHandler from "express-async-handler";
+import asyncHandler from "express-async-handler";
 
 import Product from "../models/productModel.js";
 
 // @desc    Fetch all Products
 // @route   Get /api/products
 // @access  Public
-const getProducts = AsyncHandler(async (req, res) => {
+const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
 
   res.json(products);
@@ -14,7 +14,7 @@ const getProducts = AsyncHandler(async (req, res) => {
 // @desc    Fetch Single Product
 // @route   Get /api/product/:id
 // @access  Public
-const getProductById = AsyncHandler(async (req, res) => {
+const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {

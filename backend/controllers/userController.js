5703131618c537140a43c8332c58e82436ba8bdc
mariosknl <mariosknl@outlook.com>
2020-12-04 10:29:57 +0200

    fixed import module problem

---
+++ backend/controllers/userController.js
@@ -0,0 +1,27 @@
+import asyncHandler from "express-async-handler";
+
+import User from "../models/userModel.js";
+
+// @desc    Auth user & get token
+// @route   Get /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+export { authUser };

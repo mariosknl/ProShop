099e8e26dac6316109e4c781b10537fcc23d7b99
mariosknl <mariosknl@outlook.com>
2020-12-09 10:19:58 +0200

    user login reducer & action created

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,7 +2,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-  USER_LOGIN_LOGOUT,
+  USER_LOGOUT,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -13,7 +13,7 @@ export const userLoginReducer = (state = {}, action) => {
       return { loading: false, userInfo: action.payload };
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload };
-    case USER_LOGIN_LOGOUT:
+    case USER_LOGOUT:
       return {};
     default:
       return state;
